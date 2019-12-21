daea7b3ae3897e8d64fc65082d095d7d0db099aa
Denis Bura <dendubra@MacBook-Pro-Denis.local>
2019-12-21 16:01:51 +0200

    Updates

---
+++ precache-manifest.d2e54084471801fb0c0d7206a8a7f071.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d290ce6dd44212c3683ea638ddb49d3",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "317d07f0ae839b26a887",
+    "url": "/calendar/static/css/2.80a99a59.chunk.css"
+  },
+  {
+    "revision": "df52fef00a04c009943e",
+    "url": "/calendar/static/css/main.6f1e967c.chunk.css"
+  },
+  {
+    "revision": "317d07f0ae839b26a887",
+    "url": "/calendar/static/js/2.58d440d4.chunk.js"
+  },
+  {
+    "revision": "226a37903fbb6ef33da185072d556755",
+    "url": "/calendar/static/js/2.58d440d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df52fef00a04c009943e",
+    "url": "/calendar/static/js/main.d4bd1594.chunk.js"
+  },
+  {
+    "revision": "4502050e389a259fbc2f",
+    "url": "/calendar/static/js/runtime-main.9b8c4c36.js"
+  },
+  {
+    "revision": "e1ad04b9073b58c59d433cefe09d161b",
+    "url": "/calendar/static/media/arrow.e1ad04b9.svg"
+  },
+  {
+    "revision": "28144200534b30de40d6f4f0b4164367",
+    "url": "/calendar/static/media/calendar.28144200.svg"
+  },
+  {
+    "revision": "9e083b2e3a5ea6bfccf5c7cbc2ef6cd8",
+    "url": "/calendar/static/media/home.9e083b2e.svg"
+  },
+  {
+    "revision": "9b15ce11ed6ffff1f5c13e69468566f1",
+    "url": "/calendar/static/media/search.9b15ce11.svg"
+  },
+  {
+    "revision": "01d6596864ee09a91cf4ad449b98c4c2",
+    "url": "/calendar/static/media/support.01d65968.svg"
+  }
+]);
